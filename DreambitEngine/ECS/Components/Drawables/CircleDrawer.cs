aef7e1405608a15dfd0f343785f150556e742d64
Sondirn <sondirn@gmail.com>
2026-07-24 20:35:06 -0500

    Create CircleDrawer.cs

---
+++ DreambitEngine/ECS/Components/Drawables/CircleDrawer.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Dreambit.ECS;
+
+public class CircleDrawer : DrawableComponent
+{
+    public override Rectangle Bounds => GetBounds();
+
+    public Color Color { get; set; } = Color.White;
+    public bool IsFilled { get; set; } = true;
+    public float Radius { get; set; } = 128f;
+
+    private Rectangle GetBounds()
+    {
+        var pivotToUse = Transform.WorldPosToVec2;
+
+        var pivotOffset = PivotHelper.GetRelativePivot(PivotType.Center);
+        pivotToUse -= new Vector2(pivotOffset.X * Radius, pivotOffset.Y * Radius);
+        
+        var bounds = new Rectangle(
+            (int)pivotToUse.X,
+            (int)pivotToUse.Y,
+            (int)Radius,
+            (int)Radius);
+
+        return bounds;
+    }
+
+    public override void OnDraw()
+    {
+        var center = Transform.WorldPosToVec2;
+        Core.SpriteBatch.DrawCircle(center, Radius, Color);
+    }
+}
