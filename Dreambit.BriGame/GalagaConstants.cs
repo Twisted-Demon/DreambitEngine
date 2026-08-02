0460ffc1b07de754c788a794e612db2ca8bd5f2c
Sondirn <sondirn@gmail.com>
2026-08-02 17:15:35 -0500

    Remove BriGame sample project

--- Dreambit.BriGame/GalagaConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Dreambit.BriGame;
-
-public static class GalagaConstants
-{
-    public const int GameWidth = 224;
-    public const int GameHeight = 288;
-    public const int RefreshRate = 60;
-}
